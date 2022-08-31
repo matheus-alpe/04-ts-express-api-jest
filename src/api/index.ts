7d1774d4a31cb76187b0c75ff0d920b1e882f7a4
Matheus Alves Pereira <matttalves@gmail.com>
2022-08-31 19:49:34 -0300

    feat: add path resolver

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,13 +1,13 @@
 import express from 'express';
 
-import MessageResponse from '../interfaces/MessageResponse';
+import MessageResponse from '@interfaces/MessageResponse';
 import emojis from './emojis';
 
 const router = express.Router();
 
 router.get<{}, MessageResponse>('/', (req, res) => {
   res.json({
-    message: 'API - 👋🌎🌍🌏',
+    message: 'API V1 - 👋🌎🌍🌏',
   });
 });
 
